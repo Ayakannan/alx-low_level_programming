2acc1c61fca2a6548e07fa79c2ae12ec308924d8
Ayakannan <akmk4673@mail.com>
2023-09-22 08:26:41 -0700

    a

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * print_buffer - add 2 strings.
  * @b: string1.
@@ -7,5 +8,23 @@
  */
 void print_buffer(char *b, int size)
 {
-
+	int i, j;
+    if (size <= 0) {
+        printf("\n");
+        return;
+    }
+    for (i = 0; i < size; i += 10) {
+        printf("%08x  ", i);
+        for (j = 0; j < 10 && i + j < size; j += 2) {
+            printf("%02x ", b[i + j] & 0xff);
+        }
+        for (; j < 10; j += 2) {
+            printf("   ");
+        }
+        printf(" ");
+        for (j = 0; j < 10 && i + j < size; j++) {
+            printf("%c", isprint((unsigned char)b[i + j]) ? b[i + j] : '.');
+        }
+        printf("\n");
+    }
 }
